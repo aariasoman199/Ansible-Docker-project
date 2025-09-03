089ca040f316e01abf591e5fd1bc272071197d83
arya <aariasoman1999@gmail.com>
2025-09-03 16:07:36 +0000

    changes made

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return "<h1><center>Hello from Flask in Docker + Ansible! Version3 </center></h1>"
+  return "<h1><center>Hello from Flask in Docker + Ansible! Version1 </center></h1>"
 
 if __name__ == "__main__":
     
